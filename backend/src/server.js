b282867a80a78ee05fc17f4168be503156fe8ec0
Rainyel <devrainyel@gmail.com>
2025-12-04 17:07:32 +0800

    add signup controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
-import path from "path";
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 const app = express();
@@ -12,16 +12,21 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use(express.json()) // req.body
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 // ready for deployment
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
 }
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+  connectDB();
+});
